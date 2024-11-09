866a9d07c890f3d7f482ab4b44d51d9a2b09651b
Zen <sudha.sutaschuto@gmail.com>
2024-11-10 01:00:44 +0700

    Added pictures

--- polygon_art.py
+++ polygon_art.py
@@ -2,7 +2,7 @@ import turtle
 import random
 
 class Polygon:
-    def __init__(self, num_sides, size, orientation, location, color, border_size):
+    def __init__(self, num_sides=0, size=0, orientation=0, location=None, color=None, border_size=0) -> None:
         self.num_sides = num_sides
         self.size = size
         self.orientation = orientation
@@ -17,100 +17,91 @@ class Polygon:
         turtle.color(self.color)
         turtle.pensize(self.border_size)
         turtle.pendown()
-        
         for _ in range(self.num_sides):
             turtle.forward(self.size)
             turtle.left(360 / self.num_sides)
-        
         turtle.penup()
 
-
 class EmbeddedPolygon(Polygon):
-    def __init__(self, num_sides, size, orientation, location, color, border_size, reduction_ratio, num_levels):
+    def __init__(self, num_sides, size, orientation, location, color, border_size, num_levels, reduction_ratio):
         super().__init__(num_sides, size, orientation, location, color, border_size)
-        self.reduction_ratio = reduction_ratio
         self.num_levels = num_levels
+        self.reduction_ratio = reduction_ratio
 
     def draw(self):
         current_size = self.size
         current_location = self.location
-        
         for _ in range(self.num_levels):
             super().draw()
             current_size *= self.reduction_ratio
-            self.move_to_next_position(current_size)
-
-    def move_to_next_position(self, size):
-        turtle.penup()
-        turtle.forward(size * (1 - self.reduction_ratio) / 2)
-        turtle.left(90)
-        turtle.forward(size * (1 - self.reduction_ratio) / 2)
-        turtle.right(90)
-        self.location = turtle.pos()
-
+            turtle.penup()
+            turtle.forward(self.size * (1 - self.reduction_ratio) / 2)
+            turtle.left(90)
+            turtle.forward(self.size * (1 - self.reduction_ratio) / 2)
+            turtle.right(90)
+            current_location = [turtle.xcor(), turtle.ycor()]
+            self.size = current_size
+            self.location = current_location
 
 class PolygonArt:
     def __init__(self):
-        self.screen = turtle.Screen()
-        self.screen.bgcolor("black")
         turtle.speed(0)
+        turtle.bgcolor('black')
         turtle.tracer(0)
         turtle.colormode(255)
-
-    def run(self):
-        # Get user's choice
-        choice = int(input("Which art do you want to generate? Enter a number between 1 to 9: "))
-        turtle.clear()
-
-        # Generate art based on the choice
-        if choice == 1:
-            self.draw_random_polygons(3, 20, 40)
-        elif choice == 2:
-            self.draw_random_polygons(4, 30, 50)
-        elif choice == 3:
-            self.draw_random_polygons(5, 20, 60)
-        elif choice == 4:
-            self.draw_random_polygons(6, 30, 80)
-        elif choice == 5:
-            self.draw_random_polygons(3, 10, 30)
-        elif choice == 6:
-            self.draw_embedded_polygons(4, 50, 5, 0.8)
-        elif choice == 7:
-            self.draw_embedded_polygons(5, 60, 6, 0.7)
-        elif choice == 8:
-            self.draw_embedded_polygons(6, 70, 7, 0.6)
-        elif choice == 9:
-            self.draw_embedded_polygons(3, 80, 8, 0.5)
-
-        turtle.update()
-        turtle.done()
-
-    def draw_random_polygons(self, num_sides, min_size, max_size):
-        for _ in range(15):
-            size = random.randint(min_size, max_size)
-            orientation = random.randint(0, 360)
-            location = (random.randint(-200, 200), random.randint(-200, 200))
-            color = self.get_new_color()
-            border_size = random.randint(1, 5)
-            
-            polygon = Polygon(num_sides, size, orientation, location, color, border_size)
-            polygon.draw()
-
-    def draw_embedded_polygons(self, num_sides, size, num_levels, reduction_ratio):
-        for _ in range(10):
-            orientation = random.randint(0, 360)
-            location = (random.randint(-200, 200), random.randint(-200, 200))
-            color = self.get_new_color()
-            border_size = random.randint(1, 5)
-            
-            embedded_polygon = EmbeddedPolygon(num_sides, size, orientation, location, color, border_size, reduction_ratio, num_levels)
-            embedded_polygon.draw()
 
     def get_new_color(self):
         return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
+    def run(self, choice):
+        for _ in range(random.randint(20,30)):
+            if choice == 1:
+                num_sides = 3
+            elif choice == 2:
+                num_sides = 4
+            elif choice == 3:
+                num_sides = 5
+            elif choice == 4:
+                num_sides = random.choice([3, 4, 5])
+            elif choice == 5:
+                num_sides = 3 
+            elif choice == 6:
+                num_sides = 4  
+            elif choice == 7:
+                num_sides = 5 
+            else:
+                num_sides = random.randint(3, 5)
 
-# Run the program
-if __name__ == "__main__":
-    art = PolygonArt()
-    art.run()
+            size = random.randint(50, 150)
+            orientation = random.randint(0, 90)
+            location = [random.randint(-300, 300), random.randint(-200, 200)]
+            color = self.get_new_color()
+            border_size = random.randint(1, 10)
+
+            if choice in [5, 6, 7, 8]:
+                num_levels = 3 
+            elif choice == 9:
+                num_levels = random.randint(1, 3)
+            else:
+                num_levels = 1
+
+            reduction_ratio = 0.618
+
+            if choice in [5, 6, 7, 8, 9]:
+                polygon = EmbeddedPolygon(num_sides, size, orientation, location, color, border_size, num_levels, reduction_ratio)
+            else:
+                polygon = Polygon(num_sides, size, orientation, location, color, border_size)
+
+            polygon.draw()
+
+        turtle.update()
+        turtle.done()
+
+
+art = PolygonArt()
+choice = int(input("Which art do you want to generate? Enter a number between 1 to 9 inclusive: "))
+
+if 1 <= choice <= 9:
+    art.run(choice)
+else:
+    print("Invalid choice. Please enter a number between 1 and 9.")
